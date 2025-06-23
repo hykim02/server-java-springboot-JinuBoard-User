fc43767f9c1bc0bae6b952bfc059d3ce2f1be7d1
hykim02 <hykim4435@naver.com>
2025-06-23 16:33:05 +0900

    Initial commit

---
+++ src/test/java/com/example/JinuBoard/User/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JinuBoard.User;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
